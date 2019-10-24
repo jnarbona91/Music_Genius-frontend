3447b12e0f625cf8091d2fdc6b9aa666e1271a43
Katya Neumann <katya.neumann@me.com>
2019-10-23 21:55:31 -0700

    query headset now grabs the headset id from the response

--- src/components/Cortex.js
+++ src/components/Cortex.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Websocket from "react-websocket";
 import { Button } from "reactstrap"
+// import { threadId } from "worker_threads";
+
 
 
 export default class Cortex extends React.Component{
     constructor(props) {
         super(props);
         this.state = {
-            token: "",
-            headset: ""
+            token: "", //storing cortext token for authentication
+            method: "", //storing the last request method so we know how to handle the response
+            headset: "" //storing the headset id
           };
         this.handleData = this.handleData.bind(this);
     }
@@ -18,24 +21,33 @@ export default class Cortex extends React.Component{
     }
 
     sendHello(){
+        this.state.method = "getCortexInfo"
+
         let msg = {
             "jsonrpc": "2.0",
             "method": "getCortexInfo",
             "id":1
             }
+
         this.refWebSocket.sendMessage(JSON.stringify(msg));
+        
     }
 
     getUserLogin(){
+        this.state.method = "getUserLogin"
+
         let msg = {
             "jsonrpc": "2.0",
             "method": "getUserLogin",
             "id":1
             }
         this.refWebSocket.sendMessage(JSON.stringify(msg));
+
     }
 
     getRequestAccess(){
+        this.state.method = "requestAccess"
+
         let msg = {
             "id": 1,
             "jsonrpc": "2.0",
@@ -50,6 +62,8 @@ export default class Cortex extends React.Component{
     }
 
     getAuthentication(){
+        this.state.method = "authorize"
+        
         let msg = {
             "id": 1,
             "jsonrpc": "2.0",
@@ -64,6 +78,8 @@ export default class Cortex extends React.Component{
     }
 
     queryHeadsets(){
+        this.state.method = "queryHeadsets"
+
         let msg = {
             "id": 1,
             "jsonrpc": "2.0",
@@ -75,24 +91,60 @@ export default class Cortex extends React.Component{
         this.refWebSocket.sendMessage(JSON.stringify(msg));
     }
 
+    connectHeadsets(){
+        this.state.method = "controlDevice"
+
+        let msg = {
+            
+                "id": 1,
+                "jsonrpc": "2.0",
+                "method": "controlDevice",
+                "params": {
+                    "command": "connect",
+                    "headset": "EPOC-*"
+                }
+            };
+        this.refWebSocket.sendMessage(JSON.stringify(msg));
+    }
+    
+
+
     handleData(data) {
+        console.log(this.state.method);
         let result = JSON.parse(data);
         console.log(result);
-
-        if (this.state.token === "" || this.state.token === undefined) {
-            this.state.token = result.result.cortexToken;
-            console.log("received token = " + result.result.cortexToken)
-          }
-        //   if (result.result.cortexToken != undefined && (this.state.headset === "" || this.state.headset === undefined)) {
-        //       this.state.headset = result.result.
-        //   }
+        switch(this.state.method){
+            case "requestAccess":
+                    console.log("Requesting access... please check your cortext app!")
+                    break;
+            case "authorize":
+                    this.state.token = result.result.cortexToken;
+                    console.log("received token = " + result.result.cortexToken)
+                    break;
+            case "queryHeadsets":
+                if (result.result.length > 0)
+                {
+                    this.state.headset = result.result[0].id;
+                    console.log("headset id is: " + this.state.headset);
+                }
+                else{
+                    this.state.headset = "";
+                    console.log("no headsets found");
+                }
+                    
+                break;
+        }
       }
 
-      connectHeadset(){
-          let msg = {
-
+      controlDevice(command) {
+          let result = JSON.parse(command);
+          console.log(result);
+          if (this.state.headset === "" || this.state.headset === undefined) {
+              this.state.headset = ""
           }
       }
+
+     
 
     render() {
         return (
@@ -112,8 +164,17 @@ export default class Cortex extends React.Component{
              <Button onClick={() => this.getUserLogin()}>User Login</Button>
              <Button onClick={() => this.getRequestAccess()}>Request Access</Button>
              <Button onClick={() => this.getAuthentication()}>Authorize</Button>
+             <br>
+             </br>
            
              <Button onClick={() => this.queryHeadsets()}>Find Headset</Button>
+             <br>
+             </br>
+
+             <Button onClick={() => this.controlDevice()}>Connect Headset</Button>
+             <h2>Set your sensetivity level</h2>
+             
+             <Button>Sensetivity Nob</Button>
             </div>
         )
     };
