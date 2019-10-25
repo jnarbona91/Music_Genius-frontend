0e18247a05f4ebd696f89782db77fa696324bb56
jnarbona91 <jnarbona91@gmail.com>
2019-10-25 13:13:23 -0700

    added a graph component

---
+++ src/components/Graph.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as d3 from 'd3'
+
+export default class Graph extends React.Component{
+  render(){
+    return(
+      <h1>The Graph Will Go Here</h1>
+    )
+  }
+}
