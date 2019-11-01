a4a4b8886e8134c905493fad77021d89799763c4
jnarbona91 <jnarbona91@gmail.com>
2019-11-01 10:42:59 -0700

    wip: updating graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -4,20 +4,30 @@ import * as d3 from 'd3'
 export default class Graph extends React.Component{
   componentDidMount(){
     const { eng, exc, str, rel, int, foc, } = this.props
-    const performanceMetrics = [ eng, exc, str, rel, int, foc]
+    const performanceMetrics = [ eng, 60, exc, 60, str, 60, rel, 60, int, 60, foc, 60]
     this.drawBarChart(performanceMetrics)
   }
+
+  componentDidUpdate(prevProps){
+    const { eng, exc, str, rel, int, foc, } = this.props
+    const currentProps = [ eng, exc, str, rel, int, foc]
+    console.log(prevProps)
+    if(prevProps != currentProps){
+      this.drawBarChart(currentProps)
+    }
+  }
+
   drawBarChart(performanceMetrics){
     const svgGraph = d3.select(this.refs.graph)
       .append("svg")
-      .attr("width", 150)
+      .attr("width", 500)
       .attr("height", 150)
       .style("border", "1px solid black")
     svgGraph.selectAll("rect")
       .data(performanceMetrics).enter()
         .append("rect")
         .attr("width", 30)
-        .attr("height", (datapoint) => datapoint)
+        .attr("height", (datapoint) => datapoint * 1000)
         .attr("fill", "blue")
         .attr("x", (datapoint, iteration) => iteration * 40)
         .attr("y", (datapoint) => 150 - datapoint  )
