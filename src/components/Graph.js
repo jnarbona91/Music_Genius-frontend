621180a76657569a422a8b6d6ff4aec1cef564dc
jnarbona91 <jnarbona91@gmail.com>
2019-11-13 10:50:06 -0800

    graphing

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -6,6 +6,12 @@ import { selector } from 'postcss-selector-parser';
 export default class Graph extends React.Component{
   constructor(props){
     super(props)
+      this.state={
+        performance: {
+          eng: '',
+          exc: ''
+        }
+      }
     this.drawBarChart = this.drawBarChart.bind(this);
   }
 
@@ -24,16 +30,41 @@ export default class Graph extends React.Component{
     const svgGraph = d3.select(this.refs.graph)
       .append("svg")
       .attr("width", 500)
-      .attr("height", 150)
-      .style("border", "1px solid black")
+      .attr("height", 170)
+      
+
+    const xScale = d3.scaleBand()
+      .range([60, 480])
+      .domain(["eng", "exc", "int", "rel", "foc", 'str'])
+      .padding(1)
+
+    svgGraph.append('g')
+      .attr('class', 'axis x')
+      .call(d3.axisTop(xScale))
+      .selectAll('text')
+      .attr('dy', null)
+      .attr("transform", "translate(-30, 150)")  
+      
+    const yScale = d3.scaleLinear()
+      .range([150, 10])
+      .domain([0, 1.0])
+   
+    svgGraph.append('g')
+      .attr('class', 'axis y')
+      .call(d3.axisRight(yScale))
+      .selectAll('text')
+      .attr("transform", "translate(0," + 150 + ")")
+      .attr("transform", "translate(20, 0)rotate(0)")
+    
+    
     svgGraph.selectAll("rect")
       .data(performance).enter()
         .append("rect")
         .attr("width", 15)
-        .attr("height", (datapoint) => datapoint * 1000)
+        .attr("height", (datapoint) => datapoint * 100)
         .attr("fill", "gray")
-        .attr("x", (datapoint, iteration) => iteration * 40)
-        .attr("y", (datapoint) => 150 - datapoint  )
+        .attr("x", datapoint => xScale(datapoint))
+        .attr("y", (datapoint) => 150 - datapoint  ) 
     svgGraph.selectAll('rect')
       .data(performance).exit().remove()
   }
@@ -41,8 +72,10 @@ export default class Graph extends React.Component{
   render(){
     return(
       <div>
+      <center>
       <h1>The Graph Will Go Here</h1>
       <div ref="graph"></div>
+      </center>
       </div>
     )
   }
